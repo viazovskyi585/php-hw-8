d84938c384eb429a80887c1dc15c0ad3ce53efa2
evgeniy <vyazovsky.ea@gmail.com>
2022-12-18 16:15:14 +0200

    public controllers

---
+++ src/app/Controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+class About
+{
+	public function index()
+	{
+		echo "<h1>This is about page</h1>";
+	}
+
+	public function clients_list()
+	{
+		echo "<h1>This is clients list page</h1>";
+	}
+}
