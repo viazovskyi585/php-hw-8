59046f90c011a774b1a6fb575d3184e210330bde
evgeniy <vyazovsky.ea@gmail.com>
2022-12-25 21:54:10 +0200

    home

---
+++ src/public/views/social/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Home</title>
+</head>
+
+<body>
+
+	<form action="/" method="get">
+		<label for="fname">First name:</label>
+		<br>
+		<input type="text" id="fname" name="fname">
+		<br>
+		<label for="lname">Last name:</label>
+		<br>
+		<input type="text" id="lname" name="lname">
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+
+</body>
+
+</html>
